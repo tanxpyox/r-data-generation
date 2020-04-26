e8fafbf0998898c5de38a9c881993289319ad82f
亶父 <tanxpyox@gmail.com>
2020-04-26 13:42:40 +0800

    Update highR2.R

--- highR2.R
+++ highR2.R
@@ -6,10 +6,10 @@ for (i in 1:length(onlineb)){
   distx=abs(rv[[1]]-online[i])
   meany=(2*rv[[2]]+scap[i])/3
   disty=abs(rv[[2]]-scap[i])
-  for (j in 1:4){
+  for (j in 1:9){ #Target sample size:100, generating 9 extra data points from each point in the principal data set
     tx=-1
     ty=-1
-    while (tx<0||tx>30||ty<0||ty>30){
+    while (tx<0||tx>30||ty<0||ty>30){ # Reject data point if exceed prescribed range
       tx<-rnorm(1,meanx,distx/4)
       ty<-rnorm(1,meany,disty/4)
     }
@@ -20,6 +20,3 @@ for (i in 1:length(onlineb)){
 reg<-lm(scap~online)
 print(c(summary(reg)$r.squared,summary(reg)$coefficients["online","Pr(>|t|)"]))
 effect_plot(reg, pred = online, interval = TRUE, plot.points = TRUE,x.label="Online Forum Usage (h/wk)",y.label = "Social Capital Score")
-
-
-
