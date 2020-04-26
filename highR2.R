5bbc1d62f69499a53890fd65363bd2d5eff30725
亶父 <tanxpyox@gmail.com>
2020-04-26 21:29:26 +0800

    Update highR2.R

--- highR2.R
+++ highR2.R
@@ -2,15 +2,15 @@ online <- onlineb
 scap <- scapb
 for (i in 1:length(onlineb)){
   rv<-nearestPointOnSegment(p,c(online[1],scap[1]))
-  meanx=(2*rv[[1]]+online[i])/3
+  meanx=(2*rv[[1]]+online[i])/3 #biased mean
   distx=abs(rv[[1]]-online[i])
-  meany=(2*rv[[2]]+scap[i])/3
+  meany=(2*rv[[2]]+scap[i])/3 #biased mean
   disty=abs(rv[[2]]-scap[i])
   for (j in 1:9){ #Target sample size:100, generating 9 extra data points from each point in the principal data set
     tx=-1
     ty=-1
     while (tx<0||tx>30||ty<0||ty>30){ # Reject data point if exceed prescribed range
-      tx<-rnorm(1,meanx,distx/4)
+      tx<-rnorm(1,meanx,distx/4) #biased sd
       ty<-rnorm(1,meany,disty/4)
     }
     online<-c(online,tx)
