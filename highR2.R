018d4667e0df183925ebfa04f3bd19b420bbe7a0
亶父 <tanxpyox@gmail.com>
2020-04-26 21:40:21 +0800

    declare global variables

--- highR2.R
+++ highR2.R
@@ -1,16 +1,18 @@
+pred <- function(x){return(summary(reg)$coefficient[[1]] + x * summary(reg)$coefficient[[2]])}
+p <- cbind(c(0,30),c(pred(0),pred(30)))
 online <- onlineb
 scap <- scapb
 for (i in 1:length(onlineb)){
   rv<-nearestPointOnSegment(p,c(online[1],scap[1]))
-  meanx=(2*rv[[1]]+online[i])/3 #biased mean
+  meanx=(2*rv[[1]]+online[i])/3
   distx=abs(rv[[1]]-online[i])
-  meany=(2*rv[[2]]+scap[i])/3 #biased mean
+  meany=(2*rv[[2]]+scap[i])/3
   disty=abs(rv[[2]]-scap[i])
-  for (j in 1:9){ #Target sample size:100, generating 9 extra data points from each point in the principal data set
+  for (j in 1:9){
     tx=-1
     ty=-1
-    while (tx<0||tx>30||ty<0||ty>30){ # Reject data point if exceed prescribed range
-      tx<-rnorm(1,meanx,distx/4) #biased sd
+    while (tx<0||tx>30||ty<0||ty>30){
+      tx<-rnorm(1,meanx,distx/4)
       ty<-rnorm(1,meany,disty/4)
     }
     online<-c(online,tx)
